8ce572e4b675dd9b504cba59531cd5930ca87789
Emir Görkem Özdemir <60661125+emirgorkemozdemir@users.noreply.github.com>
2023-11-20 22:01:15 +0300

    Add files via upload

---
+++ For Loop Prime Number/For Loop Prime Number.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+int main()
+{
+   /* 1-50 arası tek asal sayıların
+    kaç tane olduğunu bulan uygulama*/
+    
+    for (int i = 47; i <= 50; ++i) 
+    {
+        int counter = 0;
+        for (int k=1;k<=i;++k)
+        {
+            if (i%k ==0)
+            {
+                ++counter;
+            }
+        }
+        if (counter == 2)
+        {
+            std::cout << i << "\n";
+        }
+    }
+
+
+}
+
