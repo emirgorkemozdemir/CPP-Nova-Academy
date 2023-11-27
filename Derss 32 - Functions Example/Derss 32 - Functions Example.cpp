7d87c55872cca074afc99f3a9b9502b5b675a012
Emir Görkem Özdemir <60661125+emirgorkemozdemir@users.noreply.github.com>
2023-11-27 21:54:40 +0300

    Add files via upload

---
+++ Derss 32 - Functions Example/Derss 32 - Functions Example.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+int Square(int num)
+{
+    return num * num;
+}
+
+// Girilen sayının karesini döndüren fonksiyonu oluşturun.
+int main()
+{
+    int number;
+    std::cout << "Bir sayı giriniz";
+    std::cin >> number;
+
+    int result = Square(number);
+
+    std::cout << result;
+}
