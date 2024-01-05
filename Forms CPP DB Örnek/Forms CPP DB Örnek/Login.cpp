1de7890d617c0e64ea40d663e01610f07ea6487a
Emir Görkem Özdemir <60661125+emirgorkemozdemir@users.noreply.github.com>
2024-01-05 19:11:44 +0300

    Add files via upload

---
+++ Forms CPP DB Örnek/Forms CPP DB Örnek/Login.cpp
@@ -0,0 +1,12 @@
+﻿#include "Login.h"
+#include "MyForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+void loginmain(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	FormsCPPDBÖrnek::Login loginform;
+	Application::Run(% loginform);
+}
---
+++ Forms CPP DB Örnek/Forms CPP DB Örnek/MyForm.cpp
@@ -0,0 +1,13 @@
+﻿#include "MyForm.h"
+#include "Login.h"
+
+
+using namespace System;
+using namespace System::Windows::Forms;
+void main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	FormsCPPDBÖrnek::MyForm form1;
+	Application::Run(% form1);
+}
